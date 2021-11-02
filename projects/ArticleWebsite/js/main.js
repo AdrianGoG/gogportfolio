bc4caced33c4884d2d47d3587fb7cfc175de55e0
AdrianGoG <77414263+AdrianGoG@users.noreply.github.com>
2021-11-02 16:16:38 +0200

    Add files via upload

--- projects/ArticleWebsite/js/main.js
+++ projects/ArticleWebsite/js/main.js
@@ -1,1 +1,8 @@
-
+function myFunction() {
+  var x = document.getElementById("navi");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+} 
